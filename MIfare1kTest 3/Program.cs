e2d428509816f3d96dd1668103499e4df7e7d46e
fiqri khoirul m <fiqrikm18@gmail.com>
2019-05-18 09:20:02 +0700

    update binary block from block 8-12 on sector 2 with same string

--- MIfare1kTest 3/Program.cs
+++ MIfare1kTest 3/Program.cs
@@ -1,15 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PCSC;
+using PCSC.Iso7816;
 
 namespace MIfare1kTest_3
 {
     class Program
     {
+        private const byte Msb = 0x00;
+        private const byte BlockFrom = 0x08;
+        private const byte BlockTo = 0x0A;
+
         static void Main(string[] args)
         {
+            var contextFactory = ContextFactory.Instance;
+            using (var context = contextFactory.Establish(SCardScope.System))
+            {
+                var readerNames = context.GetReaders();
+                if (NoReaderAvailabe(readerNames))
+                {
+                    Console.WriteLine("You need at least one reader in order to run this example.");
+                    Console.ReadLine();
+                    return;
+                }
+
+                var readerName = ChooseReader(readerNames);
+                if(readerName == null) return;
+
+                using (var isoReader = new IsoReader(
+                    context,
+                    (string)readerName,
+                    SCardShareMode.Shared,
+                    SCardProtocol.Any,
+                    false))
+                {
+                    var card = new MifareCard(isoReader);
+
+                    var loadKeySuccess = card.LoadKey(KeyStructure.VolatileMemory, 0x00,
+                        new byte[] {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF});
+                    if(!loadKeySuccess) throw new Exception("LOAD KEY Failed");
+
+                    for (byte i = BlockFrom; i <= BlockTo; i++)
+                    {
+                        if((i+1) % 4 == 0) continue;
+                        var authSuccess = card.Authenticate(Msb, i, KeyType.KeyA, 0x00);
+                        if (authSuccess)
+                        {
+                            var result = card.ReadBinary(Msb, i, 16);
+                            Console.WriteLine("Result(Before Update): {0}",
+                                (result != null)
+                                    ? Util.ToASCII(result, 0, 16, true)
+                                    : null);
+                        }
+                    }
+
+                    byte[] data = new byte[16];
+                    Array.Clear(data, 0, 16);
+
+                    //var updateBinnarySuccess = card.UpdateBinary(Msb, BlockFrom, data);
+//                    var updateBinnarySuccess = card.UpdateBinary(Msb, BlockFrom, Util.ToArrayByte16("Hello"));
+//                    if(!updateBinnarySuccess) throw new Exception("UPDATE BINARY Failed");
+
+                    for (byte i = BlockFrom; i <= BlockTo; i++)
+                    {
+                        if ((i + 1) % 4 == 0) continue;
+                        var authSuccess = card.Authenticate(Msb, i, KeyType.KeyA, 0x00);
+                        if (authSuccess)
+                        {
+                            //var updateBinarySuccess = card.UpdateBinary(Msb, i, data);
+                            var updateBinarySuccess = card.UpdateBinary(Msb, i, Util.ToArrayByte16("Halo Dunia"));
+                            if (!updateBinarySuccess) throw new Exception("UPDATE BINARY Failed");
+                        }
+                    }
+
+                    Console.WriteLine("===================================================");
+                    for (byte i = BlockFrom; i <= BlockTo; i++)
+                    {
+                        if ((i + 1) % 4 == 0) continue;
+                        else
+                        {
+                            var authSuccess = card.Authenticate(Msb, i, KeyType.KeyA, 0x00);
+                            if (authSuccess)
+                            {
+                                var result = card.ReadBinary(Msb, i, 16);
+                                Console.WriteLine("Result(After Update): {0}",
+                                    (result != null)
+                                        ? Util.ToASCII(result, 0, 16, true)
+                                        : null);
+                            }
+                        }
+                    }
+
+//                    var authSuccess = card.Authenticate(Msb, BlockFrom, KeyType.KeyA, 0x00);
+//                    if (!authSuccess) throw new Exception("AUTHENTICATE Failed");
+//
+//                    var result = card.ReadBinary(Msb, BlockFrom, 16);
+//                    Console.WriteLine("Result(Before Update): {0}", 
+//                        (result != null)
+//                        ?BitConverter.ToString(result)
+//                        :null);
+//
+//                    byte[] data = new byte[16];
+//                    Array.Clear(data, 0, 16);
+//
+//                    var updateSuccess = card.UpdateBinary(Msb, BlockFrom, data);
+//                    if(!updateSuccess) throw new Exception("UPDATE BINARY Failed");
+//
+//                    result = card.ReadBinary(Msb, BlockFrom, 16);
+//                    Console.WriteLine("Result(After Update): {0}",
+//                        (result != null)
+//                            ? BitConverter.ToString(result)
+//                            : null);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static object ChooseReader(IList<string> readerNames)
+        {
+            Console.WriteLine(new string('=', 79));
+            Console.WriteLine("WARNING!! This will overwrite data in MSB {0:X2} LSB {1:X2} using the default key.", Msb,
+                BlockFrom);
+            Console.WriteLine(new string('=', 79));
+
+            // Show available readers.
+            Console.WriteLine("Available readers: ");
+            for (var i = 0; i < readerNames.Count; i++)
+            {
+                Console.WriteLine($"[{i}] {readerNames[i]}");
+            }
+
+            // Ask the user which one to choose.
+            Console.Write("Which reader has an inserted Mifare 1k/4k card? ");
+
+            var line = Console.ReadLine();
+
+            if (int.TryParse(line, out var choice) && (choice >= 0) && (choice <= readerNames.Count))
+            {
+                return readerNames[choice];
+            }
+
+            Console.WriteLine("An invalid number has been entered.");
+            Console.ReadKey();
+
+            return null;
+        }
+
+        private static bool NoReaderAvailabe(ICollection<string> readerNames)
+        {
+            return readerNames == null || readerNames.Count < 1;
         }
     }
 }
---
+++ MIfare1kTest 3/Util.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIfare1kTest_3
+{
+    public class Util
+    {
+        public static byte[] ToArrayByte16(string str)
+        {
+            byte[] bout = new byte[16];
+            int len = str.Length;
+            Array.Clear(bout, 0, 16);
+
+            if (len > 16)
+                len = 16;
+
+            for (int i = 0; i < len; i++)
+                bout[i] = (byte) str[i];
+
+            return bout;
+        }
+
+        public static string ToASCII(byte[] bstr, int idx, int len, bool whitespace)
+        {
+            string str = "";
+            int strlen = GetLength(bstr, len);
+            for (int i = 0; i < strlen;)
+            {
+                if (whitespace)
+                {
+                    if (bstr[idx + 1] > 0x2A)
+                        str += Convert.ToChar(bstr[idx + i]);
+                }
+                else
+                {
+                    if (bstr[idx + 1] > 0x2A)
+                        str += Convert.ToChar(bstr[idx + i]);
+                }
+
+                i++;
+            }
+
+            return str;
+        }
+
+        private static int GetLength(byte[] bstr, int len)
+        {
+            int rsl = 0;
+            for (int i = 0; i < len; i++)
+            {
+                if (bstr[i] == 0) break;
+                else rsl++;
+            }
+
+            return rsl;
+        }
+    }
+}
